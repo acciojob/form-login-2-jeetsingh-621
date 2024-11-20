992d11ac0d10d2418a0e73667876a592cd78a0a9
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-20 22:36:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+  function submitForm() {
+      // Get form values
+      const firstName = document.getElementById('firstName').value;
+      const lastName = document.getElementById('lastName').value;
+      const phoneNumber = document.getElementById('phoneNumber').value;
+      const emailId = document.getElementById('emailId').value;
+
+      // Generate alert message
+      const message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`;
+      alert(message);
+    }
